e92bddd898c72021a8ab94c270b1c45f3383d048
kilimichenkoDaniel <kilimichenko_D@itstep.org>
2024-11-25 19:09:16 +0200

    create new lines

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -5,18 +5,5 @@ int main()
 {
 	cout << "Hello world\n";
 	cout << "Hello world22222\n";
-	cout << "Add some text from github1\n";
-	cout << "Add some text from github1\n";
-	cout << "Add some text from github1\n";
-	cout << "Add some text from github1\n";
-	cout << "Add some text from github1\n";
-	cout << "Add some text from github1\n";
-	cout << "Add some text from github1\n";
-	cout << "Add some text from github1\n";
-	cout << "Add some text from github1\n";
-	cout << "Add some text from github1\n";
-	cout << "Add some text from github1\n";
-	cout << "Add some text from github1\n";
-	cout << "Add some text from github1\n";
-	cout << "Add some text from github1\n";
+
 }
