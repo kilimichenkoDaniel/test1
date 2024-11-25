a2aa041f277308cafc6d73d449a6509f40804582
kilimichenkoDaniel <kilimichenko_D@itstep.org>
2024-11-25 19:02:49 +0200

    Update Source.cpp

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -5,4 +5,18 @@ int main()
 {
 	cout << "Hello world\n";
 	cout << "Hello world22222\n";
+	cout << "Add some text from github1\n";
+	cout << "Add some text from github1\n";
+	cout << "Add some text from github1\n";
+	cout << "Add some text from github1\n";
+	cout << "Add some text from github1\n";
+	cout << "Add some text from github1\n";
+	cout << "Add some text from github1\n";
+	cout << "Add some text from github1\n";
+	cout << "Add some text from github1\n";
+	cout << "Add some text from github1\n";
+	cout << "Add some text from github1\n";
+	cout << "Add some text from github1\n";
+	cout << "Add some text from github1\n";
+	cout << "Add some text from github1\n";
 }
